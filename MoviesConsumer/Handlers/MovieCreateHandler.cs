0590938ed79cd303bec1b5dcdc95632b98face8a
Matt <solomatias321@gmail.com>
2024-03-25 12:44:46 +0100

    set up wolvering and rabbit in api

---
+++ MoviesConsumer/Handlers/MovieCreateHandler.cs
@@ -0,0 +1,19 @@
+﻿using MessagingContracts;
+
+namespace MoviesConsumer.Handlers
+{
+    public class MovieCreateHandler
+    {
+        private readonly ILogger<MovieCreateHandler> _logger;
+
+        public MovieCreateHandler(ILogger<MovieCreateHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Handle(MovieCreated movieCreated)
+        {
+            _logger.LogInformation(movieCreated.ToString());//only test porpuses
+        }
+    }
+}
